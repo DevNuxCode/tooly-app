72f7dd83576e94b50e43fecffaa244e6c3fcd55b
DevNuxCode <devnuxcode@gmail.com>
2024-08-14 02:55:10 -0400

    1
    1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,14 +8,14 @@ type Repo = {
 }
 
 export default async function Page(){
-    const res = await fetch('https://laravel.distribuidoravega.cl/wp-json/wp/v2/posts/51');
+    const res = await fetch('https://laravel.distribuidoravega.cl/wp-json/wp/v2/posts/');
     const data: Repo = await res.json();
     console.log(data)
     return (
                 <div className="pb-5 pt-6 text-center">
-                    <p>{data.content.rendered}</p>
+                    <p>titulo</p>
                     <p>id: {data.id}</p>
-                    <p>title: {data.title.rendered}</p>
+                    <p>title: {data.title}</p>
                     
                     
                 
